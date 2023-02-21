9c70be27a7fd25dabec3cf16c69a9ac2588601be
Daniil Rassadin <daniil@rassadin.org>
2023-02-21 20:12:08 +0600

    Task 10 done

--- task010.py
+++ task010.py
@@ -6,3 +6,19 @@
 
 # 5 -> 1 0 1 1 0
 # 2
+
+from random import randint
+
+number_of_coins = int(input("Number of coins: "))
+number_of_one, number_of_zero = 0, 0
+
+for i in range(number_of_coins):
+    coin = randint(0, 1)
+    print(coin, end=" ")
+    if coin == 1:
+        number_of_one += 1
+    elif coin == 0:
+        number_of_zero += 1
+
+print(
+    f"\nMinimum number of coins to flip is {min(number_of_one, number_of_zero)}.")
